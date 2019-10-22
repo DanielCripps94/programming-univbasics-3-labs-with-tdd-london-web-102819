59d4c01f76d5cab36fb564f12c758372b8864e54
Daniel Cripps <dan-cripps@hotmail.co.uk>
2019-10-22 14:11:46 +0000

    Automatically backed up by Learn

--- calculator.rb
+++ calculator.rb
@@ -1,5 +1,6 @@
 first_number = 1 
 
+<<<<<<< HEAD
 second_number = 3
 
 sum = second_number + first_number
@@ -9,3 +10,5 @@ difference = first_number - second_number
 product = first_number * second_number
 
 quotient = first_number / second_number
+=======
+>>>>>>> 48e60c74c736b0d1e8efeca7c94f24987570fac5
